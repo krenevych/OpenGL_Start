4ef3485a06ab839a9841a0076e5162b9db63f05a
Andrii Krenevych <krenevych@knu.ua>
2026-04-02 15:05:36 +0300

    First triangle

--- src/main.cpp
+++ src/main.cpp
@@ -33,20 +33,94 @@ int main(void)
 
     glClearColor(1.0, 0.0, 0.0, 1.0);
 
+    auto vertexShaderCode = R"(
+        #version 330 core
+
+        layout(location = 0) in vec4 aPos;
+
+        void main() {
+            gl_Position = aPos;
+        }
+    )";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, nullptr);
+    glCompileShader(vertexShader);
+
+    auto fragmentShaderCode = R"(
+        #version 330 core
+        out vec4 FragColor;
+
+        void main() {
+            FragColor = vec4(0.0, 1.0, 0.0, 1.0);  // зелений
+       }
+    )";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Програма з шейдерів
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {  // float* vertices
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+          0.f,  0.5f,
+    };
+
+    GLuint VBO; // data - ідентифікатор для даних - місток CPU та GPU
+    GLuint VAO; // vertex array object
+
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind = activate
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,                  // location - 0
+        2,                  // 2 компоненти: x, y
+        GL_FLOAT,           // тип даних
+        GL_FALSE,           // не нормалізувати
+        2 * sizeof(float),  // stride: 2 float-а на вершину
+        (void*)0            // offset: починаємо з 0
+    );
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0); // деактивувати VAO
+
+
+
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE))
+    do
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        ///// gl-команди для налаштування інструментів малювання та малювання
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
-    }
+    } while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE));
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
 
     glfwTerminate();
     return 0;

9b1656675130ad51be497cf351c835d172cec08c
Andrii Krenevych <krenevych@knu.ua>
2026-04-09 15:00:39 +0300

    Fix CMake

--- src/lesson02/main.cpp
+++ src/lesson02/main.cpp
@@ -4,19 +4,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shader_utils.h"
 
-std::string LoadShaderFromFile(const std::string &filePath) {
-    std::ifstream shaderFile(filePath);
-    if (!shaderFile.is_open()) {
-        std::cerr << "Не вдалося відкрити файл: " << filePath << std::endl;
-        return "";
-    }
 
-    std::stringstream buffer;
-    buffer << shaderFile.rdbuf(); // зчитує весь вміст файлу в потік
-
-    return buffer.str(); // повертає як std::string
-}
+// std::string LoadShaderFromFile(const std::string &filePath) {
+//     std::ifstream shaderFile(filePath);
+//     if (!shaderFile.is_open()) {
+//         std::cerr << "Не вдалося відкрити файл: " << filePath << std::endl;
+//         return "";
+//     }
+//
+//     std::stringstream buffer;
+//     buffer << shaderFile.rdbuf(); // зчитує весь вміст файлу в потік
+//
+//     return buffer.str(); // повертає як std::string
+// }
 
 int main(void)
 {
